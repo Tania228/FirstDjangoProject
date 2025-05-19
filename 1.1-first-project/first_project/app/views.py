f7b6182acffaadc1bf54dd88ed7847d8734f2956
Nenasheva Tatiana <nenasheva-tanya05@mail.ru>
2025-05-19 14:48:25 +0300

    Initial commit

--- 1.1-first-project/first_project/app/views.py
+++ 1.1-first-project/first_project/app/views.py
@@ -1,19 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render, reverse
+import os
+from datetime import datetime, time
 
 
 def home_view(request):
     template_name = 'app/home.html'
-    # впишите правильные адреса страниц, используя
-    # функцию `reverse`
+
     pages = {
         'Главная страница': reverse('home'),
-        'Показать текущее время': '',
-        'Показать содержимое рабочей директории': ''
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
     }
-    
-    # context и параметры render менять не нужно
-    # подбробнее о них мы поговорим на следующих лекциях
+
     context = {
         'pages': pages
     }
@@ -21,15 +20,19 @@ def home_view(request):
 
 
 def time_view(request):
-    # обратите внимание – здесь HTML шаблона нет, 
-    # возвращается просто текст
-    current_time = None
-    msg = f'Текущее время: {current_time}'
+    current_time = datetime.now()
+    msg = f'Текущее время: {current_time.time()}'
     return HttpResponse(msg)
 
 
 def workdir_view(request):
-    # по аналогии с `time_view`, напишите код,
-    # который возвращает список файлов в рабочей 
-    # директории
-    raise NotImplemented
+    try:
+        list_files = os.listdir(path='.')
+        files = ', '.join(list_files)
+        return HttpResponse(f'Список файлов в рабочей директории:\n{files}')
+    except Exception as e:
+        return f'Произошла ошибка {e}'
+
+
+
+
